ff49f90779b8306449c92eb9428af0d14c4609f3
lukaabesadze <167683282+lukaabesadze@users.noreply.github.com>
2024-07-25 17:33:03 +0400

    Add project files.

---
+++ ReviewApp-InWebApi/Model/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewApp_InWebApi.Model
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
